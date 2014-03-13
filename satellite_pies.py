a9b5ecc52f479e4dfaf65e638f4b23dc2f038247
hugovk <hugovk@users.noreply.github.com>
2014-03-13 12:01:47 +0200

    pep8 and pyflakes

--- satellite_pies.py
+++ satellite_pies.py
@@ -6,7 +6,6 @@ import os
 from PIL import Image, ImageOps
 import platform
 import re
-import socket
 from subprocess import Popen, PIPE
 import sys
 import tempfile
@@ -15,7 +14,9 @@ import urllib
 # Prerequisites: PIL and OSM Viz
 # Prerequisites: For --repeat, twisted.internet
 
-# Optional prerequisite for WinXP: Windows XP needs WirelessNetConsole [1] to get location based on nearby wifi networks, otherwise it will be based on your public IP address which is less accurate.
+# Optional prerequisite for WinXP: Windows XP needs WirelessNetConsole [1] to
+# get location based on nearby wifi networks, otherwise it will be based on
+# your public IP address which is less accurate.
 # [1] http://www.nirsoft.net/utils/wireless_net_console.html
 
 # TODO Error handling: no wifi, no internet...
@@ -25,17 +26,22 @@ import urllib
 try:
     from osmviz.manager import PILImageManager, OSMManager
 except ImportError, e:
-    sys.exit("ImportError: %s.\n"
-              "OSM Viz module needed, available from\n"
-              "http://cbick.github.com/osmviz/\n\n" % str(e))
+    sys.exit(
+        "ImportError: %s.\n"
+        "OSM Viz module needed, available from\n"
+        "http://cbick.github.com/osmviz/\n\n" % str(e))
 
-last_pos_id = [] # Can be a list of wifis or an IP address. Used to check if user is in the same place.
-last_coords = None,None
+ # Can be a list of wifis or an IP address.
+ # Used to check if user is in the same place.
+last_pos_id = []
+
+last_coords = None, None
+
 
 def get_placename_geolocation(name):
     name = name.replace(" ", "+")
     url = "http://nominatim.openstreetmap.org/search?format=xml&q=" + name
-    
+
     try:
         html = urllib.urlopen(url).read()
         lat = re.compile("lat='([-0-9\.]+)' ").findall(html)[0]
@@ -47,13 +53,14 @@ def get_placename_geolocation(name):
         sys.exit("Place not found")
         return None
 
+
 def get_xp_wifi_list():
     """
-    Get Windows XP's nearest wifi networks. 
+    Get Windows XP's nearest wifi networks.
     Requires WirelessNetConsole.exe.
     Returns:
         List: of nearest networks' SSIDs
-        List: of nearest networks' MAC addressses
+        List: of nearest networks' MAC addresses
         List: of nearest networks' RSSIs
     """
     print "Get WinXP wifi networks"
@@ -71,9 +78,10 @@ def get_xp_wifi_list():
     except:
         return None, None, None
 
+
 def get_nonxp_wifi_list():
     """
-    Get non-Windows XP's nearest wifi networks. 
+    Get non-Windows XP's nearest wifi networks.
     Returns:
         List: of nearest networks' SSIDs
         List: of nearest networks' MAC addressses
@@ -90,10 +98,11 @@ def get_nonxp_wifi_list():
         ssids = re.compile('SSID [0-9]* : (.*)\r').findall(stdout)
         macs = re.compile('BSSID [0-9]*                 : (.*)\r').findall(stdout)
         rssis = re.compile(' Signal             : (.*)\r').findall(stdout)
-        
+
         return ssids, macs, rssis
     except:
         return None, None, None
+
 
 def get_wifi_list():
     """
@@ -109,7 +118,7 @@ def get_wifi_list():
         ssids, macs, rssis = get_xp_wifi_list()
     else:
         ssids, macs, rssis = get_nonxp_wifi_list()
-    
+
     global last_pos_id
     number_same = len(set(last_pos_id) & set(macs))
     if number_same > 1:
@@ -119,7 +128,7 @@ def get_wifi_list():
         last_pos_id = macs
         return True, ssids, macs, rssis
 
-    
+
 def get_wifi_geolocation(ssids, macs, rssis):
     """
     Get the computer's geolocation from wifi networks. Should be very accurate.
@@ -127,11 +136,12 @@ def get_wifi_geolocation(ssids, macs, rssis):
         Tuple: latitude and longitude coordinates
     """
     print "Get wifi-based geolocation"
-    
+
     geourl = 'https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&sensor=true'
-    for i,ssid in enumerate(ssids):
+    for i, ssid in enumerate(ssids):
         print macs[i], ssid, rssis[i]
-        geourl += '&wifi=mac:%s%%7Cssid:%s%%7Css:%s' % (macs[i], ssid.replace(" ", "%20"), rssis[i])
+        geourl += '&wifi=mac:%s%%7Cssid:%s%%7Css:%s' % (
+            macs[i], ssid.replace(" ", "%20"), rssis[i])
     # Look up lat/lon from Google Maps API
     try:
         html = urllib.urlopen(geourl).read()
@@ -142,7 +152,8 @@ def get_wifi_geolocation(ssids, macs, rssis):
         return (lat, lon)
     except:
         return None
-    
+
+
 def get_ip():
     """
     Get the computer's public-facing IP address.
@@ -162,16 +173,17 @@ def get_ip():
         last_pos_id = ip
         return True, ip
 
+
 def get_ip_geolocation(ip):
     """
-    Get the computer's geolocation from its public-facing IP address. May not be very accurate, may even be in the wrong city or country.
+    Get the computer's geolocation from its public-facing IP address.
+    May not be very accurate, may even be in the wrong city or country.
     """
     print "Get IP geolocation"
     url = 'http://api.hostip.info/get_html.php?ip=%s&position=true' % ip
     print url
     response = urllib.urlopen(url).read()
     print(response)
-    lines = response.split('\n')
     lat = re.compile('Latitude: ([0-9\.]*)').search(response).group(1)
     lon = re.compile('Longitude: ([0-9\.]*)').search(response).group(1)
     # city = re.compile('City: (.*)').search(response).group(1)
@@ -182,6 +194,7 @@ def get_ip_geolocation(ip):
     # print "Country:", country
     return (lat, lon)
 
+
 def get_desktop_size():
     """
     Return computer's desktop screen size.
@@ -191,25 +204,28 @@ def get_desktop_size():
     print "Desktop size:", screensize
     return (screensize)
 
+
 def get_metres_per_pixel(lat, zoom):
     """
-    For a given latitude and OSM zoom level, return approximate number of metres each pixel represents.
+    For a given latitude and OSM zoom level,
+    return approximate number of metres each pixel represents.
     """
     metres_per_pixel = 156543.04 * math.cos(lat*(math.pi/180)) / math.pow(2, zoom)
     return metres_per_pixel
-    
-    
+
+
 def get_bounding_box((lat, lon), (height_in_metres, width_in_metres)):
     """
-    Give central coordinates and the required height/width of the screen in metres, return a bounding box of min_lat, max_lat, min_lon, max_lon.
+    Given central coordinates and the required height/width of the screen in
+    metres, return a bounding box of min_lat, max_lat, min_lon, max_lon.
     """
     # Earth's radius in metres, sphere
     R = 6378137
-    
+
     # Offsets from centre to edge, in metres
     dn = height_in_metres/2
     de = width_in_metres/2
- 
+
     # Coordinate offsets in radians
     dLat = dn/R
     dLon = de/(R*math.cos(math.pi*lat/180))
@@ -227,6 +243,7 @@ def get_bounding_box((lat, lon), (height_in_metres, width_in_metres)):
     # print min_lat, max_lat, min_lon, max_lon
     return (min_lat, max_lat, min_lon, max_lon)
 
+
 def get_osm_image((lat, lon), screensize, zoom):
     """
     Create, crop and save a PIL image of OSM tiles patched together.
@@ -237,25 +254,28 @@ def get_osm_image((lat, lon), screensize, zoom):
     height_in_metres = screensize[1] * metres_per_pixel
     # print "bbox (metres):", width_in_metres, height_in_metres
     bbox = get_bounding_box((lat, lon), (height_in_metres, width_in_metres))
-    
+
     osm = OSMManager(image_manager=PILImageManager('RGB'),
                      server=args.url_base)
     img, bnds = osm.createOSMImage(bbox, zoom)
     print "Pre-crop size: ", img.size
     img = ImageOps.fit(img, screensize, Image.ANTIALIAS)
     print "Post-crop size:", img.size
-    # outfile = os.path.join(tempfile.gettempdir(), "satellite_pies.bmp") 
+    # outfile = os.path.join(tempfile.gettempdir(), "satellite_pies.bmp")
     img.save(outfile)
     return outfile
+
 
 def set_wallpaper(filename):
     """
     Set an image as wallpaper.
     """
-    if filename != None:
+    if filename is not None:
         print filename
         SPI_SETDESKWALLPAPER = 20
-        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, filename, 0)
+        ctypes.windll.user32.SystemParametersInfoA(
+            SPI_SETDESKWALLPAPER, 0, filename, 0)
+
 
 def print_terms_of_use():
     """
@@ -265,16 +285,18 @@ def print_terms_of_use():
     print "\n * OSM maps: (c) OpenStreetMap and contributors, CC-BY-SA."
     print "\n * Watercolor and Toner maps: Courtesy of Stamen Design, and use OpenStreetMap data, (c) OpenStreetMap and contributors CC-BY-SA."
     print "\n * MapQuest Open Aerial Tiles: Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency. Tiles Courtesy of MapQuest."
-    
+
+
 def do_work():
     """
-    This gets the computer's location, fetches the correct-sized map and sets it as wallpaper.
+    This gets the computer's location, fetches the
+    correct-sized map and sets it as wallpaper.
     """
-    if args.coords: # the user specified coords
+    if args.coords:  # the user specified coords
         latlon = args.coords
-    elif  args.name: # the user specified a place name
+    elif args.name:  # the user specified a place name
         latlon = get_placename_geolocation(args.name)
-    else: # get the user's location
+    else:  # get the user's location
         changed, ssids, macs, rssis = get_wifi_list()
         if not changed:
             return
@@ -287,7 +309,8 @@ def do_work():
             latlon = get_ip_geolocation(ip)
 
             if not args.zoom:
-                args.zoom = 10 # less zoom for less accurate IP-based geolocation
+                # Less zoom for less accurate IP-based geolocation
+                args.zoom = 10
 
         global last_coords
         if last_coords == latlon:
@@ -297,7 +320,7 @@ def do_work():
             last_coords = latlon
 
     if not args.zoom:
-        args.zoom = 17 # http://wiki.openstreetmap.org/wiki/Zoom_levels
+        args.zoom = 17  # http://wiki.openstreetmap.org/wiki/Zoom_levels
 
     if args.tile == "aerial" and args.zoom > 11:
         # args.zoom = 11 # Levels 12+ are provided only in the US
@@ -308,34 +331,44 @@ def do_work():
     set_wallpaper(filename)
     print_terms_of_use()
 
+
 def argparse_coords(s):
     try:
         lat, lon = map(float, s.split(','))
         return lat, lon
     except:
         raise argparse.ArgumentTypeError("Coordinates must be lat,lon")
-        
+
 if __name__ == '__main__':
     if platform.system() != "Windows":
         sys.exit("Currently only implemented for Windows")
 
-    parser = argparse.ArgumentParser(description='Satellite pies.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-t', '--tile', default='watercolor', 
-        choices=('toner', 'watercolor', 'osm', 'aerial'), 
+    parser = argparse.ArgumentParser(
+        description='Satellite pies.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-t', '--tile', default='watercolor',
+        choices=('toner', 'watercolor', 'osm', 'aerial'),
         help='Type of map tile to use')
-    parser.add_argument('-u', '--url_base', metavar='URL', 
+    parser.add_argument(
+        '-u', '--url_base', metavar='URL',
         help='Base URL for map tiles (use instead of --tile)')
-    parser.add_argument('-z', '--zoom', metavar='level', type=int,
+    parser.add_argument(
+        '-z', '--zoom', metavar='level', type=int,
         help='Map zoom level')
-    
-    parser.add_argument('-x', '--coords', metavar='lat,lon', type=argparse_coords,
+
+    parser.add_argument(
+        '-x', '--coords', metavar='lat,lon', type=argparse_coords,
         help='Instead of your current position, use these coordinates')
-    parser.add_argument('-n', '--name', 
+    parser.add_argument(
+        '-n', '--name',
         help='Instead of your current position, use this place name')
 
-    parser.add_argument('-r', '--repeat', action='store_true',
+    parser.add_argument(
+        '-r', '--repeat', action='store_true',
         help='Repeat')
-    parser.add_argument('-w', '--wait', metavar='minutes', type=int, 
+    parser.add_argument(
+        '-w', '--wait', metavar='minutes', type=int,
         help='Minutes to wait between repeat', default=5)
 
     args = parser.parse_args()
@@ -351,15 +384,15 @@ if __name__ == '__main__':
         elif args.tile == 'aerial':
             args.url_base = "http://oatile1.mqcdn.com/tiles/1.0.0/sat"
 
-    outfile = os.path.join(tempfile.gettempdir(), "satellite_pies.bmp") 
+    outfile = os.path.join(tempfile.gettempdir(), "satellite_pies.bmp")
     set_wallpaper(outfile)
-    
+
     if args.repeat:
         from twisted.internet import reactor, task
         loop = task.LoopingCall(do_work)
-        loop.start(args.wait * 60) # seconds
+        loop.start(args.wait * 60)  # seconds
         reactor.run()
     else:
-        do_work() # run once
+        do_work()  # run once
 
 # End of file
